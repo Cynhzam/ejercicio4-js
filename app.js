5b2e2d0c5482068c2aee915fa47f643632fc927f
Cynhzam <cyn.herrerazamudio@gmail.com>
2016-08-25 14:33:53 -0500

    cuarto ejercicio js

---
+++ app.js
@@ -0,0 +1,4 @@
+var tiempo= parseInt(prompt("Ingresa el tiempo de tu llamada"));
+var costoMinuto= parseInt(prompt("Costo por minuto"));
+
+document.write("El costo total de la llamada es:"+(tiempo*costoMinuto));
